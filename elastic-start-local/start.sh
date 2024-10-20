1ffaaec4cf9d196d48d0452ff03bbd34ac3c3fc1
Leon <l.beccard@protonmail.com>
2024-10-20 17:35:24 +0200

    Add vector database Elasticsearch

---
+++ elastic-start-local/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Start script for start-local
+# More information: https://github.com/elastic/start-local
+set -eu
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd ${SCRIPT_DIR}
+
+docker compose up --wait
