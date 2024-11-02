f6a91e7f9b0e527b20128063311aab02689abf20
MehdinMasinovic <mehdin.masinovic2@gmail.com>
2024-11-02 09:11:27 +0000

    Use argparse to choose between creating new embeddings or updating the es index

--- app/workflow.py
+++ app/workflow.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from processor import Processor
 from embedding import Embedding
 from search import Search
@@ -17,7 +18,7 @@ class Workflow:
             text = file.read()
 
         # Chunk the text
-        chunks = self.processor.chunk_text(self.text)
+        chunks = self.processor.chunk_text(text=text, max_size_kb=4)
         self.processor.save(chunks)
 
         # Generate embeddings for each chunk
@@ -32,6 +33,12 @@ class Workflow:
         return chunks, embeddings
 
 if __name__ == "__main__":
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Choose workflow operation")
+    parser.add_argument('operation', choices=['create-embeddings', 'update-es-index'], help="Operation to perform: 'create' or 'update'")
+
+    args = parser.parse_args()
+
     # English
     # model_name='jinaai/jina-embeddings-v2-small-en'
     # spacy_model = 'en_core_web_sm'
@@ -41,5 +48,8 @@ if __name__ == "__main__":
     spacy_model = 'de_core_news_lg'
     workflow = Workflow(model_name=model_name, spacy_model=spacy_model)
 
-    #workflow.create_new_embeddings()
-    workflow.update_es_index()
+    # Run the appropriate method based on user input
+    if args.operation == 'create-embeddings':
+        workflow.create_new_embeddings()
+    elif args.operation == 'update-es-index':
+        workflow.update_es_index()
