38f4e6c5c38d9cd126788bd30e50696f097c7270
MehdinMasinovic <mehdin.masinovic2@gmail.com>
2024-11-02 07:47:47 +0000

    Keep embedding model as object to Embedding class

--- app/embedding.py
+++ app/embedding.py
@@ -8,10 +8,12 @@ from sentence_transformers import SentenceTransformer
 from processor import Processor
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = 'max_split_size_mb:256'
 
 class Embedding:
     def __init__(self, model_name):
-        pass
+        self.model = AutoModel.from_pretrained(model_name, trust_remote_code=True,
+                                               torch_dtype=torch.bfloat16)
 
     def main(self):
 
@@ -19,31 +21,26 @@ class Embedding:
         chunks = Processor().chunk_text(self.text, spacy_model='de_core_news_lg')
 
         # Generate embeddings for each chunk
-        embeddings = self.create_embeddings(chunks, model_name)
+        embeddings = self.create_embeddings(chunks)
 
         # Store chunks and embeddings
         self.save_chunks(chunks)
         self.save_embeddings(embeddings)
 
 
-    def create_embeddings(self, chunks, model_name, batch_size=8):
+    def create_embeddings(self, chunks, batch_size=8):
         """Generate embeddings for each chunk using Sentence-BERT."""
 
         # Catch edge case if there are no chunks
         if not chunks:
             return []
 
-        os.environ["PYTORCH_CUDA_ALLOC_CONF"] = 'max_split_size_mb:256'
-
-        model = AutoModel.from_pretrained(model_name, trust_remote_code=True,
-                                          torch_dtype=torch.bfloat16)
-
         embeddings = []
         with torch.no_grad():  # Disable gradient calculations
             for i in range(0, len(chunks), batch_size):
                 batch_chunks = chunks[i:i + batch_size]
                 with autocast():  # Enable mixed precision
-                    batch_embeddings = model.encode(batch_chunks)  # Generate embeddings for the batch
+                    batch_embeddings = self.model.encode(batch_chunks)  # Generate embeddings for the batch
                 embeddings.extend(batch_embeddings)  # Append batch embeddings to the main list
 
                 # Clear CUDA cache after each batch to free memory
@@ -52,29 +49,18 @@ class Embedding:
         return embeddings
 
     @staticmethod
-    def save_chunks(chunks):
-        with open(os.path.join(FILE_PATH, '../data/chunks.pkl'), 'wb') as f:
-            pickle.dump(chunks, f)
-
-    @staticmethod
-    def save_embeddings(embeddings):
+    def save(embeddings):
         np.save(os.path.join(FILE_PATH, '../data/embeddings.npy'), embeddings)
 
     @staticmethod
-    def load_chunks(path= os.path.join(FILE_PATH, '../data/chunks.pkl')):
-        with open(path, 'rb') as f:
-            chunks = pickle.load(f)
-        return chunks
-
-    @staticmethod
-    def load_embeddings(path= os.path.join(FILE_PATH, '../data/embeddings.npy')):
+    def load(path= os.path.join(FILE_PATH, '../data/embeddings.npy')):
         embeddings = np.load(path)
         return embeddings
 
 if __name__ == "__main__":
     # model_name='jinaai/jina-embeddings-v2-small-en'
     model_name = 'jinaai/jina-embeddings-v2-base-de'
-    embedding = Embeddings(model_name=model_name)
+    embedding = Embedding(model_name=model_name)
     embedding.main()
 
 
