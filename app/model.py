05481931c48afc32d6df16defe7ec64c333ccf51
Mehdin Masinovic <mehdin.masinovic2@gmail.com>
2025-01-16 15:31:34 +0000

    Pass the Search() class directly to the Model() class

--- app/model.py
+++ app/model.py
@@ -1,14 +1,11 @@
 import ollama
-from search import Search
-
 class Model:
 
     def chat(self, question):
         response = ollama.chat(model='llama3.2', messages=[{ 'role': 'user', 'content': question }])
         return response['message']['content']
 
-    def rag(self, question):
-        es = Search()
+    def rag(self, question, es):
         retrieved_text = es.search(question)
         response = ollama.chat(model='llama3.2', messages=[{ 'role': 'user', 'content': f"Du hast folgende Information: {retrieved_text}. \n Welcher Teil davon ist relevant, um folgende Frage zu beantworten: {question}. " }])
         return response['message']['content']
