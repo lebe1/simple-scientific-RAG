556ec1990ece80e836f8853359134cee8b19bb36
Mehdin Masinovic <mehdin.masinovic2@gmail.com>
2025-01-17 13:01:25 +0000

    Keep the TXT and PKL version of the chunk files

--- app/processor.py
+++ app/processor.py
@@ -37,13 +37,13 @@ class Processor:
         return chunks
 
     def save(self, chunks):
-        file_name = f'../data/chunks-{self.chunk_size_in_kb}kb.pkl'
+        file_name = f'../data/{self.spacy_model}_{self.chunk_size_in_kb}kb_chunks.pkl'
         path = os.path.join(FILE_PATH, file_name)
         with open(path, 'wb') as f:
             pickle.dump(chunks, f)
 
     def load(self):
-        file_name = f'../data/chunks-{self.chunk_size_in_kb}kb.pkl'
+        file_name = f'../data/{self.spacy_model}_{self.chunk_size_in_kb}kb_chunks.pkl'
         path = os.path.join(FILE_PATH, file_name)
         with open(path, 'rb') as f:
             chunks = pickle.load(f)
