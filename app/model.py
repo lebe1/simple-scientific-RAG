e020dd315dcd05c7b402ad8aaa5756202ecb84be
Leon <l.beccard@protonmail.com>
2025-03-28 18:35:14 +0100

    Improve reproducibility in LLM generated answers by setting temperature to 0 and changing prompt content

--- app/model.py
+++ app/model.py
@@ -9,5 +9,5 @@ class Model:
         print(f"Accessing RAG system...")
         retrieved_text = es.search(question)
         print(f"Passing the text to the LLM...")
-        response = ollama.chat(model='llama3.2', messages=[{ 'role': 'user', 'content': f"Du hast folgende Information: {retrieved_text}. \n Welcher Teil davon ist relevant, um folgende Frage zu beantworten: {question}. " }])
+        response = ollama.chat(model='llama3.2', options={'temperature': 0.0}, messages=[{ 'role': 'user', 'content': f"Du hast folgende Information: {retrieved_text}. \n Antworte auf die folgende Frage sehr präzise in nur einem Satz und mit so wenigen Worten wie möglich. {question}" }])
         return response['message']['content']
