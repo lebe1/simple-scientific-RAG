53645fdf6f1edca7b2c50c7ac6a4a359ea072c27
Leon <l.beccard@protonmail.com>
2025-04-23 10:24:52 +0200

    Enable updating index automatically after creating embedding

--- app/workflow.py
+++ app/workflow.py
@@ -12,7 +12,7 @@ class Workflow:
         self.embedding = Embedding(spacy_model=spacy_model, chunk_size_in_kb=chunk_size_in_kb, model=model)
         self.es = Search(embedding=self.embedding)
 
-    def create_new_embeddings(self, split_by_article=False, split_by_subarticle=False):
+    def create_new_embeddings(self, split_by_article=False, split_by_subarticle=False, index_after_creation=True):
         # Read the legal basis text
         legal_text_path = os.path.join(FILE_PATH, DATA_PATH)
         with open(legal_text_path, 'r', encoding='utf-8') as file:
@@ -30,6 +30,9 @@ class Workflow:
         # Generate embeddings for each chunk
         embeddings = self.embedding.create_embeddings(chunks)
         self.embedding.save(embeddings)
+
+        if index_after_creation:
+            self.es.index_chunks(chunks, embeddings, self.embedding.index_name)
 
     def update_es_index(self):
         chunks = self.processor.load()
